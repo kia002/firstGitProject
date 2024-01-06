c3a9af49e118770104ddc773fd2bc3081dacd876
Илья Курочкин <ilya.kurochkin.02@mail.ru>
2024-01-06 19:36:02 +0500

    Change sout

--- src/main/java/FirstGit.java
+++ src/main/java/FirstGit.java
@@ -2,7 +2,7 @@ public class FirstGit {
     public static void main(String[] args) {
         System.out.println("GIT DEVELOP");
         System.out.println("LEARNING!");
-        System.out.println("It's terribly difficult");
+        System.out.println("It's terribly difficult, oh no");
         System.out.println("I am happy!");
         System.out.println("!!!!");
     }
