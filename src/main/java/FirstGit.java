a8baca6a8bc7e8640d2698a9501f7df954715e9b
kia002 <80974079+kia002@users.noreply.github.com>
2024-01-06 17:45:53 +0500

    Add symbol to sout FirstGit

--- src/main/java/FirstGit.java
+++ src/main/java/FirstGit.java
@@ -1,6 +1,6 @@
 public class FirstGit {
     public static void main(String[] args) {
         System.out.println("GIT");
-        System.out.println("LEARNING");
+        System.out.println("LEARNING!");
     }
 }
